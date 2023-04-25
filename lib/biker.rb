35b8929446b8a1a7e7df690b55787fd7cb3db464
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-25 09:30:38 -0600

    add rides and terrain

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,8 +1,16 @@
 class Biker
   attr_reader :name,
-              :max_distance
+              :max_distance,
+              :rides,
+              :acceptable_terrain
   def initialize(name, max_distance)
     @name = name
     @max_distance = max_distance
+    @rides = {}
+    @acceptable_terrain = []
+  end
+
+  def learn_terrain!(terrain)
+    @acceptable_terrain << terrain
   end
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,6 +4,8 @@ require './lib/biker'
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   describe '#initialize' do
@@ -14,6 +16,20 @@ RSpec.describe Biker do
     it 'has attributes' do
       expect(@biker.name).to eq("Kenny")
       expect(@biker.max_distance).to eq 30
+    end
+  end
+
+  describe '#rides and terrain' do
+    it 'starts with no rides and acceptable terrain' do
+      expect(@biker.rides).to eq ({})
+      expect(@biker.acceptable_terrain).to eq([])
+    end
+
+    it 'can learn terrain' do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+
+      expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
     end
   end
 end
