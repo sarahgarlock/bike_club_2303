155b72c1babcfc14ac3f9e39c577ec370a5b643b
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-25 09:17:28 -0600

    finish Ride tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,6 +3,7 @@ require './lib/ride'
 RSpec.describe Ride do
   before(:each) do
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   describe '#initialize' do
@@ -15,7 +16,14 @@ RSpec.describe Ride do
       expect(@ride1.distance).to eq 10.7
       expect(@ride1.terrain).to eq(:hills)
       expect(@ride1.loop?).to eq false
-      expect(@ride1.total_distance).to eq(21.4)
+      expect(@ride1.total_distance).to eq 21.4
+    end
+  end
+
+  describe '#loop' do
+    it 'can give a total distance for a loop' do
+      expect(@ride2.loop?).to be true
+      expect(@ride2.total_distance).to be 14.9
     end
   end
 end
