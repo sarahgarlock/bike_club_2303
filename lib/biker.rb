0dc8355a6ffed24e58d3e9a750a684776e81336d
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-25 11:51:11 -0600

    adjust attr reader in Biker class

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,5 +1,8 @@
 class Biker
-  attr_reader :name, :max_distance, :acceptable_terrain, :rides
+  attr_reader :name, 
+              :max_distance, 
+              :acceptable_terrain, 
+              :rides
 
   def initialize(name, max_distance)
     @name = name
