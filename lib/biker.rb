dc1021cc09a3aa0c81925a24f96050b9b9f23a25
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-25 09:48:28 -0600

    add personal records

--- lib/biker.rb
+++ lib/biker.rb
@@ -13,4 +13,16 @@ class Biker
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
   end
+
+  def log_ride(ride, distance)
+    if @rides.include?(ride)
+      @rides[ride] << distance
+    else
+      @rides[ride] = [distance]
+    end
+  end
+
+  def personal_record(ride)
+    @rides[ride].min
+  end
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,6 +4,7 @@ require './lib/biker'
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
@@ -30,6 +31,32 @@ RSpec.describe Biker do
       @biker.learn_terrain!(:hills)
 
       expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+  end
+
+  describe '#log rides' do
+    it 'can log rides into a hash' do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.rides).to eq({
+              @ride1 => [92.5, 91.1],
+              @ride2 => [60.9, 61.6]
+            })
+    end
+  end
+
+  describe '#personal record' do
+    it 'can select fastest time from rides' do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.personal_record(@ride1)).to eq 91.1
+      expect(@biker.personal_record(@ride2)).to eq 60.9
     end
   end
 end
